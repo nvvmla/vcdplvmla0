9e89d2e8ff4cc6faac743ea4d405e785b759ec3d
nvvmla <nv.vmla@hotmail.com>
2026-08-23 12:00:27 +0630

    Update default UUID in index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || '81960df1-9f0f-4727-bcbd-014cd6cb09f9';
+const UUID = process.env.UUID || '9298bab1-7ffa-463f-a749-005747c89820';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
